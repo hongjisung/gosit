1dd91656705a265496f94a7c5e7d8cce8970dc8d
jisung <rhfktj@gmail.com>
2018-08-24 04:42:10 +0900

    add index to ratioUser
    ratioUser will be used for search many times

--- server/database/dbcreatetable.sql
+++ server/database/dbcreatetable.sql
@@ -1,8 +1,8 @@
 -- initiate
-drop table seoulBusData;
+--drop table seoulBusData;
 
--- 서울 버스 노선에 대한 전체 정보를 저장하는 테이블
--- 중간에 에러가 나서 idx추가했는데 다른거 때문에 그랬엇는데 안없앴음 없애도 되는데 안없애도 될거같고 
+서울 버스 노선에 대한 전체 정보를 저장하는 테이블
+중간에 에러가 나서 idx추가했는데 다른거 때문에 그랬엇는데 안없앴음 없애도 되는데 안없애도 될거같고 
 create table seoulBusData(
     routeId varchar(10),
     routeName varchar(20),
@@ -18,7 +18,7 @@ create table seoulBusData(
 
 -- 서울 버스 노선ID 및 stationID에 해당하는 주말,공휴일 / 평일 이용객 비율
 -- initiate
-drop table ratioUser;
+-- drop table ratioUser;
 
 create table ratioUser(
     routeId varchar(10),
@@ -27,7 +27,8 @@ create table ratioUser(
     weekDayAlightRatio int,
     weekendRideRatio int,
     weekendAlightRatio int,
-    primary key(routeId)
-);
+    primary key(routeId),
+    index ratioidx (routeId)
+)charset=utf8;
 
 
