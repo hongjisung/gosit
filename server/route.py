e0d4976b41aaa79e6fb74422516906f526d71a3d
jisung <rhfktj@gmail.com>
2018-08-30 14:56:48 +0900

    not contained file above commit
    this should have been contained above commit...

--- server/route.py
+++ server/route.py
@@ -2,37 +2,60 @@ from flask import Flask, render_template,request
 from flask import url_for, redirect, make_response
 from flask_cors import CORS
 import json
-from src.getRouteData import busRoute
-from src import realtimeBusLocation
+from src import initialBusCall, realtimeBusLocation
+from src import statsBus
+
 
 app = Flask(__name__)
 CORS(app)
 
-# get route number from client
-# name is routeNum
-@app.route('/busRoute', methods=['POST','GET'])
-def findBusRoute():
+
+# initialCall protocol in ./develop_note/protocol.json
+@app.route('/initialCall', methods=['POST','GET'])
+def initialCall() :
     routeNum = 0
-    if(request.method == 'POST') :
-        routeNum = request.form['routeNum']
+    if(request.method == 'POST'):
+        routeNum = request.form['busName']
     else :
-        routeNum = request.args.get('routeNum')
-    json_data = busRoute(routeNum)
+        routeNum = request.args.get('busName')
+    json_data = initialBusCall.initialCall(routeNum)
     res = make_response(json_data)
-    res.headers['Content-Type'] = 'findBusRoute/json'
+    res.headers['Content-Type'] = 'initialCall/json'
     return res
 
+# realtimeData protocol in ./develop_note/protocol.json
 @app.route('/realtimeData', methods=['POST', 'GET'])
 def realtimeLocation() : 
     routeNum = 0
     if(request.method == 'POST'):
-        routeNum = request.form['busId']
+        routeNum = request.form['busName']
     else :
-        routeNum = request.args.get('busId')
+        routeNum = request.args.get('busName')
     json_data = realtimeBusLocation.realtimeLocation(routeNum)
     res = make_response(json_data)
     res.headers['Content-Type'] = 'realtimeLoc/json'
     return res
+
+
+# stats protocol in ./develop_note/protocol.json
+@app.route('/stats', methods=['POST', 'GET'])
+def stats() :
+    routeName = '' 
+    daytype = ''
+    hour = 0
+    if(request.method == 'POST'):
+        routeName = request.form['busName']
+        daytype = request.form['dayType']
+        hour  = request.form['hour']
+    else :
+        routeName = request.args.get('busName')
+        daytype = request.args.get('dayType')
+        hour = request.args.get('hour')
+    json_data = statsBus.stats(routeName,daytype, hour)
+    res = make_response(json_data)
+    res.headers['Content-Type'] = 'stats/json'
+    return res
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')    
